dc3ebceb8226271c70e0a242d7f70135a62f939a
HP <ngocbaopham1606@gmail.com>
2021-07-22 21:02:46 +0700

    project1

--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -11,14 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-public class HomeController implements IController {
-
-    public void process(final HttpServletRequest request, final HttpServletResponse response, final ServletContext servletContext, final ITemplateEngine templateEngine) throws Exception {
+public class MovieController implements IController {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) throws Exception {
         WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-        List<Movie> list = new MovieService().getMoviesforHomePage();
-        ctx.setVariable("list", list);
+        String id = request.getParameter("id");
+        Movie movie = new MovieService().getMovieByID(id);
+        ctx.setVariable("movie", movie);
         List<String> genres = new MovieService().getGenresforHeader();
         ctx.setVariable("genres", genres);
-        templateEngine.process("index", ctx, response.getWriter());
+        templateEngine.process("movie", ctx, response.getWriter());
     }
 }
